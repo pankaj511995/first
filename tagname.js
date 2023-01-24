883bed2d7582fbf09645c540a7e76cbaf9a2e92b
pankaj511995 <kpankaj511995@gmail.com>
2023-01-24 18:49:23 +0530

    tagelement

---
+++ tagname.js
@@ -0,0 +1,13 @@
+let item=document.getElementsByClassName('item-list')
+item[0].innerText='i am adding item form class index 0'
+item[1].innerText='i am class of index 1'
+item[2].innerText='index 2 item from class filw'
+item[3].innerText='i am index 3 in class form please considor it'
+console.log(item)
+let li=document.getElementsByTagName('li')
+li[0].textContent=" i am adding through li elemtnt in index 0"
+li[1].textContent="index 1 in added by element tag li"
+li[2].textContent="i am tag number 2 of li claass "
+li[3].textContent="i am li elemet of index number 3"
+
+console.log(li)
