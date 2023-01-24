90c17c96831ebdea6b698f82d236cb59db36e956
pankaj511995 <kpankaj511995@gmail.com>
2023-01-24 16:34:43 +0530

    index_manipulation

---
+++ findingitem.js
@@ -0,0 +1,34 @@
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.all)//depricated
+// console.log(document.images)
+// console.log(document.links)
+// console.log(document.forms)
+let header=document.getElementById('contaner')
+let h11=document.getElementsByClassName('contaner')
+let text=document.getElementById('add')
+// header.textContent='not item';
+// header.innerText='i am changing list item'
+h11.innerText=' i am class item'// by class it not working 
+// text.innerText=' Add another item'
+console.log(header)
+console.log(text)
+let inpu=document.getElementById('item')
+let sub=document.getElementById('submit')
+// inpu.value='i am pankaj kumar , i am adding new item'
+console.log(inpu)
+// sub.style.background='green'
+let addItem=document.getElementsByClassName('item-list')// selecting all list item group
+// let addItem=document.getElementById('add-item')
+
+console.log(addItem[1])
+for(let i=0;i<addItem.length;i++){
+    // addItem[i].innerText=`vary bad ${i}`
+    // addItem[i].style.background='blue'
+}
+
+// let odd=document.querySelectorAll('li: nth-child(odd)');
+// let even=document.querySelectorAll('li : nth-child(even)')
