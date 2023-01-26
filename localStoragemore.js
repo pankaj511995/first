fd80afaf07b9df74d788b9591c2a8f9a38305c2e
pankaj511995 <kpankaj511995@gmail.com>
2023-01-26 18:28:24 +0530

    LocalDelete

---
+++ localStoragemore.js
@@ -0,0 +1,14 @@
+// let form=document.querySelector('#form1')
+// let subm=document.getElementById('submit')
+// subm.addEventListener('click',addItem);
+// let email1=document.getElementById('email1')
+// let phone=document.getElementById('phone')
+// let name1=document.getElementById('item')
+
+// function addItem(e){
+//     e.preventDefault()
+//     // console.log(`${email1}`)
+//     console.log(`${name1.value} : ${email1.value} : ${phone.value}`)
+//     // console.log(email1)
+//     localStorage.setItem(`${email1.value}`,`${name1.value} : ${email1.value} : ${phone.value}`)
+// }
